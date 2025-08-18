94fd9db49961529ebdcaa43b613b213bd7f0b214
Niba <63934510+niba291@users.noreply.github.com>
2025-08-18 16:45:10 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
     integrations    : [tailwind()],
     site            : "https://niba291.github.io",
-    base            : "/Portfolio",
+    base            : "/",
 });
