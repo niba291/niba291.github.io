366ea67fb9b11b88bad602c61dbcbc6da0e0a168
niba291 <nibachavez291@gmail.com>
2026-08-04 18:04:18 -0400

    feat: add hero and adjusment scroll

--- src/data/site.ts
+++ src/data/site.ts
@@ -7,24 +7,23 @@ export const site = {
     profileImage: "https://avatars.githubusercontent.com/u/63934510?v=4",
     social: {
         github: "https://github.com/niba291",
-        linkedin: "https://www.linkedin.com/in/nibaldochzvez/",
+        linkedin: "https://www.linkedin.com/in/nibaldochavez/",
         email: "mailto:nibaldochavezp@gmail.com",
         resume: "https://drive.usercontent.google.com/download?id=1lOFNnBKkJb5xx6aNCAtzuQXdTZrmEXMW&export=download&authuser=2&confirm=t&uuid=7ab48a27-65bb-4ee4-8ff3-a0e624a7dd5e&at=APZUnTVjhrUMz75oolkr8FeFlLev:1705411041880",
     },
 };
 
 export const navLinks = [
-    { label: "Inicio", href: "#home", icon: "line-md:home" },
-    { label: "Experiencia", href: "#experiences", icon: "line-md:briefcase" },
-    { label: "Certificados", href: "#certifications", icon: "line-md:document-list" },
-    { label: "Proyectos", href: "#projects", icon: "line-md:folder" },
+    { label: "Inicio", href: "#home", icon: "solar:home-2-bold" },
+    { label: "Experiencia", href: "#experiences", icon: "solar:case-bold" },
+    { label: "Certificados", href: "#certifications", icon: "solar:documents-bold" },
+    { label: "Proyectos", href: "#projects", icon: "solar:folder-2-bold" },
 ];
 
 export const footer = {
     craftedBy: {
         name: "@niba291",
         href: "https://github.com/niba291",
-        icon: "mdi:crown",
     },
     inspiredBy: [
         { name: "Tailwind CSS", href: "https://tailwindcss.com" },
