5423bb559737909bb7fa7d1439001c40594bff0d
niba291 <nibachavez291@gmail.com>
2026-08-09 12:16:05 -0400

    fix: use observer mode for astro-reveal

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,5 @@ export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
-    integrations: [icon(), reveal()],
+    integrations: [icon(), reveal({ mode: "observer" })],
 });
